10f1bfad70ddc092ebc3c3d6c1df14cc75c38653
mrene42 <mariarenebarco.969@gmail.com>
2024-11-06 14:38:26 +0100

    Feat:Js funtion createCharacterCard

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const requestURL = "https://dragonball-api.com/api/characters?limit=100";
 
-async function fetchitemsJson() {
+async function fetchCharacterJson() {
     try{
         const response = await fetch(requestURL);
         if (!response.ok){
@@ -14,3 +14,34 @@ async function fetchitemsJson() {
     }
 }
 
+function createCharacterCard({name, ki, maxKi, race, gender, image, affiliation}){
+    return`
+    <div class="card-group">
+        <div class= "card" id="allCards">
+            <img src= "${image}" class="card-img-top" alt="allCards">
+            <div class="Card-body" id="allCards"
+                <h5 class="card-title" id="allCards">${name}</h5>
+                <p class="card-text">${race} - ${gender}</p>
+                <h6 class="card-text" id="allCards">Base KI: ${ki}</h6>
+                <h6 class="card-text" id="allCards">Total KI: ${maxKi}</h6>
+                <h6 class="card-text" id="allCards">Afilliation: ${affiliation}</h6>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+async function  displayItems() {
+    const CharactersSection = document.getElementById("itemsSection")
+    const itemsData = await fetchCharacterJson();
+
+    if (itemsData && itemsData.items){
+        const itemsCards = itemsData.items.map(createCharacterCard).join('');
+        CharactersSection.innerHTML = itemsCards;
+    }
+    else{
+        CharactersSection.innerHTML = `<p>No se ha podido cargar el JSON de los Characters</p>`;
+    }
+}
+
+displayItems();
